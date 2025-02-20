3887112e3b4460775d3d063e687e935dc4b32395
lsuryana <lucaselbert@gmail.com>
2025-02-20 11:39:56 +0100

    - Add function to calculate the reasons value based on the length of time that occurs when following another vehicle

--- main/lib/reasons_evaluation.py
+++ main/lib/reasons_evaluation.py
@@ -1,4 +1,41 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+def evaluate_time_following(reasons, DT, distance_buffer, distance_threshold, time_threshold, moving_obstacles,state, time_passed):
+    # reasons = 'driver_reasons' or 'cyclist_reasons', depending on the evaluation
+    if reasons == 'driver_reasons':
+        distance_to_obstacle = np.linalg.norm(
+            [moving_obstacles[0].get()[0] - state.x, moving_obstacles[0].get()[1] - state.y])
+        if distance_to_obstacle < (distance_threshold + distance_buffer):
+            time_passed += DT
+            # print(time_passed)
+            # print('time passed is: {}'.format(time_passed))
+            # print('distance to bicycle is: {}'.format(distance_to_obstacle))
+            if time_passed >= time_threshold:
+                reasons_driver = 1 / np.exp(0.2 * (time_passed - time_threshold))
+                return reasons_driver, time_passed
+        else:
+            reasons_driver = 1
+            return reasons_driver, time_passed
+        # Ensure the function always returns something
+        return 1, time_passed  # Default return (e.g., full compliance)
+
+    elif reasons == 'cyclist_reasons':
+        distance_to_car = np.linalg.norm(
+            [moving_obstacles[0].get()[0] - state.x, moving_obstacles[0].get()[1] - state.y])
+        if distance_to_car < (distance_threshold + distance_buffer):
+            time_passed += DT
+            print('time passed is: {}'.format(time_passed))
+            # print('distance to bicycle is: {}'.format(distance_to_obstacle))
+            if time_passed >= time_threshold:
+                reasons_cyclist = 1 / np.exp(0.4 * (time_passed - time_threshold))
+                return reasons_cyclist, time_passed
+        else:
+            reasons_cyclist = 1
+            return reasons_cyclist, time_passed
+
+        # Ensure the function always returns something
+        return 1, time_passed  # Default return (e.g., full compliance)
 
 def evaluate_distance_to_centerline(av_position, av_width, centerline_location, constant):
     # Calculate the distance from the left edge of the vehicle to the centerline
@@ -8,6 +45,6 @@ def evaluate_distance_to_centerline(av_position, av_width, centerline_location, 
     if distance >= 0:
         reasons_policymaker = 1
     else:
-        reasons_policymaker = np.exp(constant * (distance ** -2))
+        reasons_policymaker = np.exp(constant * (distance -2))
 
     return reasons_policymaker
