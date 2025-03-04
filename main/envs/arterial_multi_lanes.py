461790d6515baa1ef47fa1cb037b58bb83821804
lsuryana <lucaselbert@gmail.com>
2025-03-04 16:32:39 +0100

    Added a new obstacle that defines how far it can move based on the predicted trajectory.

--- main/envs/arterial_multi_lanes.py
+++ main/envs/arterial_multi_lanes.py
@@ -1,4 +1,7 @@
 import sys
+
+from cvxpy import length
+
 sys.path.append('..')
 
 import numpy as np
@@ -6,6 +9,7 @@ from matplotlib import pyplot as plt
 from main.lib.obstacles import BoxObstacle
 from main.lib.scenario import Scenario
 from main.lib.plot_obstacles import plot_intersection
+from main.lib.car_dimensions import CarDimensions, BicycleModelDimensions
 
 class ArterialMultiLanes:
     def __init__(self, num_lanes=2, goal_lane=1):
@@ -13,7 +17,7 @@ class ArterialMultiLanes:
         self.goal_lane = goal_lane
         self.width_road = 4
         self.width_pavement = 5
-        self.length = 100
+        self.length = 50
         self.allowed_goal_theta_difference = np.pi / 16
         self.goal_lane_adjustment = goal_lane - 1
 
@@ -35,7 +39,7 @@ class ArterialMultiLanes:
             lane_offset += self.width_road / 2
         return left_pavement, right_pavement, lane_offset
 
-    def create_scenario(self, moving_obstacles=False,
+    def create_scenario(self, moving_obstacles=False, moving_obstacles_trajectory=None,
                         spawn_location_x=None, spawn_location_y=None,
                         av_location_x=None, av_location_y=None):
         if not self.validate_lanes():
@@ -44,14 +48,23 @@ class ArterialMultiLanes:
         left_pavement, right_pavement, lane_offset = self.calculate_offsets()
         start = (self.width_road * (self.num_lanes / 2 - 0.5), -self.length / 2, np.pi/2)
         goal = (lane_offset, self.length / 2, np.pi/2)
-        goal_area = BoxObstacle(xy_width=(self.width_road, self.width_road), height=1, xy_center=(goal[0], goal[1]))
+
+        car_dimensions: CarDimensions = BicycleModelDimensions(skip_back_circle_collision_checking=False)
+        goal_area = BoxObstacle(xy_width=(car_dimensions.bounding_box_size[0], car_dimensions.bounding_box_size[1]), height=1, xy_center=(goal[0], goal[1]))
+
+        # TO SET GOAL AREA AS A BOX
+        # goal_area = BoxObstacle(xy_width=(self.width_road, self.width_road), height=1, xy_center=(goal[0], goal[1]))
 
         if moving_obstacles is True:
             start = (av_location_x, av_location_y, np.pi / 2)
+            lower_side_obstacle = moving_obstacles_trajectory[0][0][1]
+            upper_side_obstacle = moving_obstacles_trajectory[0][-1][1]
+            # to set the length of the obstacle as the sum of the length of the obstacle and the predicted trajectory
+            length_obstacle_plus_predicted_trajectory = upper_side_obstacle - lower_side_obstacle
             obstacles = [
                 BoxObstacle(xy_width=(self.width_pavement, self.length), height=1, xy_center=(left_pavement, 0)),
                 BoxObstacle(xy_width=(self.width_pavement, self.length), height=0.1, xy_center=(right_pavement, 0)),
-                BoxObstacle(xy_width=(1.64, 1.64), height=0.1, xy_center=(spawn_location_x, spawn_location_y))
+                BoxObstacle(xy_width=(1.64, length_obstacle_plus_predicted_trajectory), height=0.1, xy_center=(spawn_location_x, spawn_location_y))
             ]
         else:
             obstacles = [
