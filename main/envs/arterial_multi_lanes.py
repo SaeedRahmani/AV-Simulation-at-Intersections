e0a33ecff6bc718207423c5421f53da395c0ceba
lsuryana <lucaselbert@gmail.com>
2025-03-21 00:11:44 +0100

    Changed width_road, width_pavement, and added if-else for is_following. If is_following = False and moving_obstacles = True, then obstacles for overtaking. Otherwise, obstacles on pavements and left road.

--- main/envs/arterial_multi_lanes.py
+++ main/envs/arterial_multi_lanes.py
@@ -15,8 +15,8 @@ class ArterialMultiLanes:
     def __init__(self, num_lanes=2, goal_lane=1):
         self.num_lanes = num_lanes
         self.goal_lane = goal_lane
-        self.width_road = 4
-        self.width_pavement = 5
+        self.width_road = 3
+        self.width_pavement = 4
         self.length = 50
         self.allowed_goal_theta_difference = np.pi / 16
         self.goal_lane_adjustment = goal_lane - 1
@@ -32,8 +32,8 @@ class ArterialMultiLanes:
 
     def calculate_offsets(self):
         # to determine the location of the pavements
-        left_pavement = - (self.num_lanes * self.width_road / 2) - (self.width_pavement / 2)
-        right_pavement = (self.num_lanes * self.width_road / 2) + (self.width_pavement / 2)
+        left_pavement = - (self.num_lanes * self.width_road / 2) - (self.width_pavement / 2) - 1
+        right_pavement = (self.num_lanes * self.width_road / 2) + (self.width_pavement / 2) + 1
         lane_offset = (self.num_lanes // 2 - 0.5) * self.width_road - self.goal_lane_adjustment * self.width_road
         if self.num_lanes % 2 != 0:
             lane_offset += self.width_road / 2
@@ -41,7 +41,7 @@ class ArterialMultiLanes:
 
     def create_scenario(self, moving_obstacles=False, moving_obstacles_trajectory=None,
                         spawn_location_x=None, spawn_location_y=None,
-                        av_location_x=None, av_location_y=None):
+                        av_location_x=None, av_location_y=None, is_following=True, frame_visualization=False):
         if not self.validate_lanes():
             return None
 
@@ -55,21 +55,31 @@ class ArterialMultiLanes:
         # TO SET GOAL AREA AS A BOX
         # goal_area = BoxObstacle(xy_width=(self.width_road, self.width_road), height=1, xy_center=(goal[0], goal[1]))
 
-        if moving_obstacles is True:
-            start = (av_location_x, av_location_y, np.pi / 2)
-            lower_side_obstacle = moving_obstacles_trajectory[0][0][1]
-            upper_side_obstacle = moving_obstacles_trajectory[0][-1][1]
-            # to set the length of the obstacle as the sum of the length of the obstacle and the predicted trajectory
-            length_obstacle_plus_predicted_trajectory = upper_side_obstacle - lower_side_obstacle
+        if frame_visualization is True:
             obstacles = [
-                BoxObstacle(xy_width=(self.width_pavement, self.length), height=1, xy_center=(left_pavement, 0)),
-                BoxObstacle(xy_width=(self.width_pavement, self.length), height=0.1, xy_center=(right_pavement, 0)),
-                BoxObstacle(xy_width=(1.64, length_obstacle_plus_predicted_trajectory), height=0.1, xy_center=(spawn_location_x, spawn_location_y))
-            ]
+                BoxObstacle(xy_width=(self.width_pavement, self.length), height=1, xy_center=(left_pavement, 0)), # left pavement
+                BoxObstacle(xy_width=(self.width_pavement, self.length), height=0.1, xy_center=(right_pavement, 0))] # right pavement
         else:
-            obstacles = [
-                BoxObstacle(xy_width=(self.width_pavement, self.length), height=1, xy_center=(left_pavement, 0)),
-                BoxObstacle(xy_width=(self.width_pavement, self.length), height=0.1, xy_center=(right_pavement, 0))]
+            if moving_obstacles is True and is_following is False:
+                start = (av_location_x, av_location_y, np.pi / 2)
+                lower_side_obstacle = moving_obstacles_trajectory[0][0][1]
+                upper_side_obstacle = moving_obstacles_trajectory[0][-1][1]
+                # to set the length of the obstacle as the sum of the length of the obstacle and the predicted trajectory
+                length_obstacle_plus_predicted_trajectory = upper_side_obstacle - lower_side_obstacle
+                # remove obstacle on the left road and add obstacle on the right road with the predicted trajectory of the obstacle
+                # why divided by 2? Because the obstacle is centered at the spawn location
+                spawn_location_y_update = spawn_location_y + length_obstacle_plus_predicted_trajectory/2
+                obstacles = [
+                    BoxObstacle(xy_width=(self.width_pavement, self.length), height=1, xy_center=(left_pavement, 0)),
+                    BoxObstacle(xy_width=(self.width_pavement, self.length), height=0.1, xy_center=(right_pavement, 0)),
+                    BoxObstacle(xy_width=(1.64, length_obstacle_plus_predicted_trajectory), height=0.1, xy_center=(spawn_location_x, spawn_location_y_update))
+                ]
+            else:
+                # add three obstacles: left pavement, right pavement, and left road
+                obstacles = [
+                    BoxObstacle(xy_width=(self.width_pavement, self.length), height=1, xy_center=(left_pavement, 0)), # left pavement
+                    BoxObstacle(xy_width=(self.width_pavement, self.length), height=0.1, xy_center=(right_pavement, 0)), # right pavement
+                    BoxObstacle(xy_width=(self.width_road, self.length), height=0.1, xy_center=(-start[0], 0))] # left road
         return Scenario(
             start=start,
             goal_point=goal,
